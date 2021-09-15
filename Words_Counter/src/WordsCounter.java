3e65146acf68a1ee9f184ec62a304cec02f39bbd
DoctorONE007 <drone@pswd.ru>
2021-09-15 18:23:48 +0300

    Add %words to task 2

--- Words_Counter/src/WordsCounter.java
+++ Words_Counter/src/WordsCounter.java
@@ -7,13 +7,14 @@ import java.util.TreeMap;
 public class WordsCounter {
     Map<String, Integer> dictionary = new TreeMap<>();
     String s = "";
-
+    double count=0;
     public WordsCounter(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
         scanner.useDelimiter("[\\p{Punct}\\s0-9]+");
 
         while (scanner.hasNext()) {
             s = scanner.next().toLowerCase();
+            count++;
             if (dictionary.containsKey(s))
                 dictionary.put(s, dictionary.get(s) + 1);
             else
@@ -25,7 +26,7 @@ public class WordsCounter {
         else {
             System.out.println("Отсортированный в алфавитном порядке список слов и частота с которой они встречаются:");
             for (Map.Entry<String, Integer> entry : dictionary.entrySet())
-                System.out.println(entry.getKey() + " - " + entry.getValue());
+                System.out.println(entry.getKey() + " - " + entry.getValue() + " - "+String.format("%.2f", entry.getValue()*100/count)  + "%");
             printMaxCount();
         }
 
@@ -41,6 +42,6 @@ public class WordsCounter {
         System.out.println("Самые часто встречающиеся слова:");
         for (Map.Entry<String, Integer> entry : dictionary.entrySet())
             if (entry.getValue() == max)
-                System.out.println(entry.getKey() + " - " + entry.getValue());
+                System.out.println(entry.getKey() + " - " + entry.getValue() + " - "+ String.format("%.2f", entry.getValue()*100/count) + "%");
     }
 }
